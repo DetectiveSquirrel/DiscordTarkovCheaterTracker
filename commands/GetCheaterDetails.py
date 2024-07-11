c55bfdf6a65c60eddf7c099d9042b4ecb9a6e678
Detective Squirrel <2137769+DetectiveSquirrel@users.noreply.github.com>
2024-07-11 12:36:07 +1200

    better details layout

--- commands/GetCheaterDetails.py
+++ commands/GetCheaterDetails.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 import db.database as database
 import logging
+from helpers.utils import get_user_mention
 
 logger = logging.getLogger("bot")
 
@@ -74,13 +75,16 @@ class CheaterDetails(commands.Cog):
 
         # Add required fields
         embed.add_field(
-            name="Last Known Name", value=cheater_details["name"], inline=True
+            name="Last Known Name",
+            value=f"[{cheater_details['name']}](https://tarkov.dev/player/{cheater_details['id']})",
+            inline=True,
         )
         embed.add_field(
-            name="ID",
+            name="Profile ID",
             value=f"[{cheater_details['id']}](https://tarkov.dev/player/{cheater_details['id']})",
             inline=True,
         )
+
         # Determine last report type and details
         if (
             cheater_details.get("last_kill_report_time")
@@ -90,7 +94,7 @@ class CheaterDetails(commands.Cog):
             and cheater_details.get("last_kill_report_time")
             >= cheater_details.get("last_death_report_time")
         ):
-            last_report_type = "Kill Report"
+            last_report_type = "Murder Report"
             last_reported_time = cheater_details.get("last_kill_report_time")
             last_reported_by = cheater_details.get("last_kill_reported_by")
         else:
@@ -98,15 +102,12 @@ class CheaterDetails(commands.Cog):
             last_reported_time = cheater_details.get("last_death_report_time")
             last_reported_by = cheater_details.get("last_death_reported_by")
 
-        last_reported_user = await self.bot.fetch_user(last_reported_by)
-        last_reported_mention = (
-            last_reported_user.mention
-            if last_reported_user
-            else f"Unknown User ({last_reported_by})"
+        last_reported_mention = await get_user_mention(
+            interaction.guild, self.bot, last_reported_by
         )
         embed.add_field(
-            name=f"Last Reported ({last_report_type})",
-            value=f"{last_reported_mention} at <t:{last_reported_time}:f>",
+            name=f"Last Reported as a {last_report_type}",
+            value=f"{last_reported_mention}\n<t:{last_reported_time}:R>",
             inline=True,
         )
 
@@ -121,77 +122,63 @@ class CheaterDetails(commands.Cog):
                 else f"Unknown Server ({cheater_details['most_reported_server']['server_id']})"
             )
             embed.add_field(
-                name="Most Reported Server",
-                value=f"{server_name} ({cheater_details['most_reported_server']['count']} times)",
+                name="Most Reports From Server",
+                value=f"'{server_name}' ({cheater_details['most_reported_server']['count']})",
                 inline=False,
             )
 
         # Add kill report details if available
         if cheater_details["total_kill_reports"] > 0:
-            most_killed_by_user = await self.bot.fetch_user(
-                cheater_details["most_killed_by"]["user_id"]
-            )
-            most_killed_by_mention = (
-                most_killed_by_user.mention
-                if most_killed_by_user
-                else f"Unknown User ({cheater_details['most_killed_by']['user_id']})"
+            most_killed_by_mention = await get_user_mention(
+                interaction.guild,
+                self.bot,
+                cheater_details["most_killed_by"]["user_id"],
             )
             embed.add_field(
-                name="Total Killed",
-                value=f"{cheater_details['total_kill_reports']}",
+                name="Total Times Murdered",
+                value=f"({cheater_details['total_kill_reports']})",
                 inline=True,
             )
             embed.add_field(
                 name="Most Killed By",
-                value=f"{most_killed_by_mention} ({cheater_details['most_killed_by']['count']} times)",
+                value=f"{most_killed_by_mention}\n({cheater_details['most_killed_by']['count']})",
                 inline=True,
             )
-            last_kill_reported_user = await self.bot.fetch_user(
-                cheater_details["last_kill_reported_by"]
-            )
-            last_kill_reported_mention = (
-                last_kill_reported_user.mention
-                if last_kill_reported_user
-                else f"Unknown User ({cheater_details['last_kill_reported_by']})"
+            last_kill_reported_mention = await get_user_mention(
+                interaction.guild, self.bot, cheater_details["last_kill_reported_by"]
             )
             embed.add_field(
-                name="Last Reported By (Kill)",
-                value=f"{last_kill_reported_mention} at <t:{cheater_details['last_kill_report_time']}:f>",
+                name="Last Reported",
+                value=f"{last_kill_reported_mention}\n<t:{cheater_details['last_kill_report_time']}:R>",
                 inline=True,
             )
 
         # Add death report details if available
         if cheater_details["total_death_reports"] > 0:
             if cheater_details["most_deaths_to"]:
-                most_deaths_to_user = await self.bot.fetch_user(
-                    cheater_details["most_deaths_to"]["user_id"]
-                )
-                most_deaths_to_mention = (
-                    most_deaths_to_user.mention
-                    if most_deaths_to_user
-                    else f"Unknown User ({cheater_details['most_deaths_to']['user_id']})"
+                most_deaths_to_mention = await get_user_mention(
+                    interaction.guild,
+                    self.bot,
+                    cheater_details["most_deaths_to"]["user_id"],
                 )
                 embed.add_field(
                     name="Total Deaths From",
-                    value=f"{cheater_details['total_death_reports']}",
+                    value=f"({cheater_details['total_death_reports']})",
                     inline=True,
                 )
                 embed.add_field(
-                    name="Most Deaths To",
-                    value=f"{most_deaths_to_mention} ({cheater_details['most_deaths_to']['count']} times)",
+                    name="Most Deaths From",
+                    value=f"{most_deaths_to_mention}\n({cheater_details['most_deaths_to']['count']})",
                     inline=True,
                 )
-                last_death_reported_user = await self.bot.fetch_user(
-                    cheater_details["last_death_reported_by"]
-                )
-                last_death_reported_mention = (
-                    last_death_reported_user.mention
-                    if last_death_reported_user
-                    else f"Unknown User ({cheater_details['last_death_reported_by']})"
+                last_death_reported_mention = await get_user_mention(
+                    interaction.guild,
+                    self.bot,
+                    cheater_details["last_death_reported_by"],
                 )
                 embed.add_field(
-                    name="Last Reported By (Death)",
-                    value=f"{last_death_reported_mention} at <t:{cheater_details['last_death_report_time']}:f>",
+                    name="Last Reported",
+                    value=f"{last_death_reported_mention}\n<t:{cheater_details['last_death_report_time']}:R>",
                     inline=True,
                 )
 
